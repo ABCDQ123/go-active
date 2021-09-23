5ff310cadc72a791f983cf5eab3a8870c91434e2
wml <18981745898@163.com>
2021-09-23 10:19:02 +0800

    ..

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module go-active
 
 go 1.17
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+)
+
+//module语句指定包的名字（路径）
+//require语句指定的依赖项模块
+//replace语句可以替换依赖项模块
+//exclude语句可以忽略依赖项模块
--- learn_mysql/learn_mysql.go
+++ learn_mysql/learn_mysql.go
@@ -3,38 +3,52 @@ package learn_mysql
 import (
 	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"time"
 )
 
 const (
-	DATABASE  = "mysql"
 	USER_NAME = "root"
 	PASS_WORD = "root"
 	NET       = "tcp"
 	HOST      = "localhost"
 	PORT      = "3306"
+	DATABASE  = "mysql"
 	CHARSET   = "utf8"
 )
 
-var mysql_db *sql.DB
-var mysql_err error
+var sql_db *sql.DB
+var sql_err error
 
 func sql_open() {
 	sql_string := fmt.Sprintf("%s:%s@%s(%s:%s)/%s?charset=%s", USER_NAME, PASS_WORD, NET, HOST, PORT, DATABASE, CHARSET)
-	mysql_db, mysql_err = sql.Open(DATABASE, sql_string)
-	if mysql_err != nil {
-		fmt.Println("mysql Sql Open err", mysql_err.Error())
+	sql_db, sql_err = sql.Open(DATABASE, sql_string)
+	if sql_err != nil {
+		fmt.Println("mysql Sql Open err", sql_err.Error())
 		return
 	}
-	mysql_db.SetMaxOpenConns(100)
-	mysql_db.SetMaxIdleConns(20)
-	mysql_db.SetConnMaxLifetime(time.Second * 100)
-	mysql_err = nil
-	if mysql_err = mysql_db.Ping(); mysql_err != nil {
-		panic("mysql Sql ping err: " + mysql_err.Error())
+	sql_db.SetMaxOpenConns(10)
+	sql_db.SetMaxIdleConns(10)
+	sql_db.SetConnMaxLifetime(time.Minute)
+	sql_err = nil
+	if sql_err = sql_db.Ping(); sql_err != nil {
+		panic("mysql Sql ping err: " + sql_err.Error())
 	}
 }
 
 func sql_table_create() {
-
+	//**exec query prepare
+	//sql_db.Exec()
+	//sql_db.Query()
+	//stm ,err :=sql_db.Prepare()
+	//stm.Exec()
+	//stm.Query()
+	//**bein
+	//tx, err :=sql_db.Begin()
+	//tx.Exec()
+	//tx.Query()
+	//stm ,err =tx.Prepare()
+	//stm.Exec()
+	//stm.Query()
+	//tx.Commit()
 }
